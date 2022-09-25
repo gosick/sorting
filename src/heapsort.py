819687a18fe31bef95307bbd730169a778cccb95
Jimmy Huang <jimmy.huang@scantrader.com>
2022-09-25 16:10:49 +0800

    add min heap and fix testing case

--- src/heapsort.py
+++ src/heapsort.py
@@ -3,7 +3,8 @@
 class HeapSort:
     """
         heap sort with simple list
-        sort using max heap
+        max_sort using max heap
+        min_sort using min heap
     """
 
     def __init__(self, input_list):
@@ -24,7 +25,7 @@ class HeapSort:
             self._input_list[largest], self._input_list[root] = self._input_list[root], self._input_list[largest]
             self._max_heap(largest, heap_size)
 
-    def sorting(self):
+    def max_sorting(self):
         size = len(self._input_list)
         for index in range(size // 2 - 1, -1, -1):
             self._max_heap(index, size)
@@ -32,6 +33,30 @@ class HeapSort:
         for item in range(size - 1, 0, -1):
             self._input_list[0], self._input_list[item] = self._input_list[item], self._input_list[0]
             self._max_heap(0, item)
+
+    def _min_heap(self, root, heap_size):
+        left = root * 2 + 1
+        right = root * 2 + 2
+        smallest = root
+
+        if left < heap_size and self._input_list[left] < self._input_list[smallest]:
+            smallest = left
+        
+        if right < heap_size and self._input_list[right] < self._input_list[smallest]:
+            smallest = right
+        
+        if smallest != root:
+            self._input_list[smallest], self._input_list[root] = self._input_list[root], self._input_list[smallest]
+            self._min_heap(smallest, heap_size)
+
+    def min_sorting(self):
+        size = len(self._input_list)
+        for index in range(size // 2 - 1, -1, -1):
+            self._min_heap(index, size)
+
+        for item in range(size - 1, 0, -1):
+            self._input_list[0], self._input_list[item] = self._input_list[item], self._input_list[0]
+            self._min_heap(0, item)
 
     def get_input_list(self):
         return self._input_list
--- test/test_heapsort.py
+++ test/test_heapsort.py
@@ -3,33 +3,69 @@ import src.heapsort as heapsort
 
 class TestHeapSort(unittest.TestCase):
 
-    def test_fixed_case(self):
-
+    def test_fixed_case_with_max_heap(self):
+        import copy
         origin = [30, 20, 20, 21, 15, 1, 10, 5]
-        print("input:", origin)
-        h = heapsort.HeapSort(origin)
-        h.sorting()
+        input = copy.copy(origin)
+        print("input:", input)
+        h = heapsort.HeapSort(input)
+        h.max_sorting()
         output = h.get_input_list()
         print("output:", output)
-        self.assertEqual(origin, output)
+        self.assertFalse(origin == output)
+        # sort from small to large
+        origin.sort()
+        self.assertTrue(origin == output)
 
-    def test_random_case(self):
-
-        def generate_random(nums=100, max_int=150):
-            output_list = []
-            from random import randint
-            for _ in range(nums):
-                ele = randint(0, max_int)
-                output_list.append(ele)
-            return output_list
-
-        origin = generate_random()
-        print("input:", origin)
-        h = heapsort.HeapSort(origin)
-        h.sorting()
+    def test_fixed_case_with_min_heap(self):
+        import copy
+        origin = [30, 20, 20, 21, 15, 1, 10, 5]
+        input = copy.copy(origin)
+        print("input:", input)
+        h = heapsort.HeapSort(input)
+        h.min_sorting()
         output = h.get_input_list()
         print("output:", output)
-        self.assertEqual(origin, output)
+        self.assertFalse(origin == output)
+        # sort from large to small
+        origin.sort(reverse=True)
+        self.assertTrue(origin == output)
+
+    def generate_random(self, nums=100, max_int=150):
+        output_list = []
+        from random import randint
+        for _ in range(nums):
+            ele = randint(0, max_int)
+            output_list.append(ele)
+        return output_list
+
+    def test_random_case_with_max_heap(self):
+        import copy
+        origin = self.generate_random()
+        input = copy.copy(origin)
+        print("input:", input)
+        h = heapsort.HeapSort(input)
+        h.max_sorting()
+        output = h.get_input_list()
+        print("output:", output)
+        self.assertFalse(origin == output)
+        # sort from small to large
+        origin.sort()
+        self.assertTrue(origin == output)
+
+    def test_random_case_with_min_heap(self):
+        import copy
+        origin = self.generate_random()
+        input = copy.copy(origin)
+        print("input:", input)
+        h = heapsort.HeapSort(input)
+        h.min_sorting()
+        output = h.get_input_list()
+        print("output:", output)
+        self.assertFalse(origin == output)
+        # sort from large to small
+        origin.sort(reverse=True)
+        self.assertTrue(origin == output)
 
 
 if __name__ == '__main__':
